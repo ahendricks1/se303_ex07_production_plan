30392113af721f28de24f45e5eb286398767302d
Austin Hendricks <hendraus@oregonstate.edu>
2021-05-16 09:49:24 -0700

    Add no producers data to setup

--- test/province_test.rb
+++ test/province_test.rb
@@ -8,6 +8,13 @@ class ProvinceTest < Minitest::Test
 
   def setup
     @asia = Province.new(sample_province_data)
+    @no_producers_data = {
+      name: "No producers",
+      producers: [],
+      demand: 30,
+      price: 20
+    }
+    @no_producers = Province.new(@no_producers_data)
   end
 
   def test_province_shortfall
